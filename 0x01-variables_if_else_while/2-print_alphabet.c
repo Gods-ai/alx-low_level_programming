d576e73783d826494a4fff03953aa778b5e9c548
Godswill <onwusilikenonso@gmail.com>
2023-02-19 14:17:26 -0800

    print lower cases

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -11,8 +13,7 @@ int main(void)
 	char c;
 
 	for (c == 'a'; c <= 'z'; c++)
-{
-	putchar(c);
-}
+		putchar(c);
+		putchar('\n');
 	return (0);
 }
