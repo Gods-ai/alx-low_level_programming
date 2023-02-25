e17a724894326fd6e4ff41b2a2ce54cad5a8c4ba
Godswill <onwusilikenonso@gmail.com>
2023-02-25 08:18:34 -0800

    prints 0-9 without 2and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most number 0-9 withoout 2 and 4
+ *
+ *
+ * Return: None since we are void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
