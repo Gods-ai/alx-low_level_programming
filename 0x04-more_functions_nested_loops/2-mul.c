f6c9caafbb4e27c10778ef8885c80b782fb4b785
Godswill <onwusilikenonso@gmail.com>
2023-02-25 07:13:42 -0800

    multiples two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - mutiplies two integers
+ * @a: represent integer value
+ * @b: rrepresents b integer value
+ *
+ * Return: Always the 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	/* declarung a variable to store the result */
+	int result = 0;
+
+	result = a * b;
+
+	return (result);
+}
