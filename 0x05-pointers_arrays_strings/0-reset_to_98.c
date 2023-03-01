34015ecf7a327cbd4e96a15eb31bc763225921e4
Godswill <onwusilikenonso@gmail.com>
2023-02-28 19:47:25 -0800

    takes a pointer *n and updates it to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98(int *n) - function takes a pointer to an interger
+ * @*n: will update the valu of n to 98
+ *
+ * Return: None since were void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
