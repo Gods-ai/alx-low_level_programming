1af0421d964237c8ba9c27b683c2f6163854c9ba
Godswill <onwusilikenonso@gmail.com>
2023-03-08 05:28:10 -0800

    made corrections for betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * *_memset - a function thst fills the memory of the pointer with n
- * @*s: the pointer where the value will be stored
+ * _memset - a function thst fills the memory of the pointer with n
+ *
+ * @s: the pointer where the value will be stored
  * @b: representing the address to be stored
  * @n: the value to be stored
  *
+ * Return: returns a pointer to @s
  */
 
 char *_memset(char *s, char b, unsigned int n)
