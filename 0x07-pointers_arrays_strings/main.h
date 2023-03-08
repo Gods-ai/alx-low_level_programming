1fa1a0715635d0242ae95d77970286c082bc2869
Godswill <onwusilikenonso@gmail.com>
2023-03-08 06:30:08 -0800

    functions thst copis n from src to dest

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -22,4 +22,5 @@ void puts2(char *str);
 void puts_half(char *str);
 char *_strcat(char *dest, char*src);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
