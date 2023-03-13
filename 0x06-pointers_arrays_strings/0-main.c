21985da021bdd4a3449b82c126423858dd5b649e
Godswill <onwusilikenonso@gmail.com>
2023-03-13 07:06:56 -0700

    strcat indeed

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "world!\n";
+	char *ptr;
+
+	printf("%s\n",s1);
+	printf("%s", 
