b4cf03ae2fb142de936e950658c2275cd4762c1f
Godswill <onwusilikenonso@gmail.com>
2023-03-13 06:03:38 -0700

    betty corrections

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,17 +11,15 @@ int _atoi(char *s)
 	int sign = 1;
 
 
-	do
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
-		else if 
+		else if
 			(*s >= '0' && *s <= '9')
-				num = (num *10) + (*s -'0');
+				num = (num * 10) + (*s - '0');
 		else if
 			(num > 0)
 				break;
-	}
-	while (*s++);
+	} while (*s++);
 	return (num * sign);
 }
